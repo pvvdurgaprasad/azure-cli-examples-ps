1febde0ff9b01473a1bda685d9fad3fb113b1d7f
durgaprasad.potnuru <85012836@bat.com>
2020-05-26 00:53:46 +0530

    Added new script

---
+++ createADGroup.ps1
@@ -0,0 +1,12 @@
+Get-Content .\site-params.config | Invoke-Expression
+
+$ADGroup
+$ADGroupDesc
+
+Connect-AzAccount
+Write-Output "Connected to Azure."
+
+New-AzADGroup -DisplayName $ADGroup -MailNickname $ADGroup -Description $ADGroupDesc -Verbose
+
+Disconnect-AzAccount -Scope Process -ErrorAction Stop | Out-Null
+Write-Output "Disconnected."
